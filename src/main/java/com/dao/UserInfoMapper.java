cb5ba92b9043bd83b30a5590772d785e8d3a8424
lenziten <lenziten@hotmail.com>
2018-02-28 10:14:13 +0800

    commit

---
+++ src/main/java/com/dao/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import com.domain.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Integer uid);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Integer uid);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
